c2000fa1ab10f30a13bdbbd6eaaaf0837a279a87
Vanh2110 <hoangvanh2110@gmail.com>
2023-07-24 20:57:17 +0700

    Add project source code

---
+++ Model/DomainClass/DanhSachNguoiDung.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giao_Dien.Model.DomainClass
+{
+    internal class DanhSachNguoiDung
+    {
+        private static DanhSachNguoiDung _instance;
+        internal static DanhSachNguoiDung Instance 
+        { 
+            get
+            {
+                if (_instance == null)
+                    _instance = new DanhSachNguoiDung();
+                return _instance;
+            } 
+            set => _instance = value; 
+        }
+
+        List<NguoiDung> _lstNguoiDung;
+
+        public DanhSachNguoiDung(List<NguoiDung> lstNguoiDung)
+        {
+            _lstNguoiDung = lstNguoiDung;
+        }
+
+        public List<NguoiDung> LstNguoiDung { get => _lstNguoiDung; set => _lstNguoiDung = value; }
+
+        private DanhSachNguoiDung()
+        {
+            LstNguoiDung = new List<NguoiDung>
+            {
+                new NguoiDung("admin1", "admin1", NguoiDung.LoaiND.admin),
+                new NguoiDung("giangvien2", "giangvien2", NguoiDung.LoaiND.giangvien),
+                new NguoiDung("sinhvien3", "sinhvien3", NguoiDung.LoaiND.sinhvien)
+            };
+        }
+    }
+}
+    
---
+++ View/FormDangNhap.cs
@@ -0,0 +1,72 @@
+﻿using Giao_Dien.Model.DomainClass;
+using Giao_Dien.View;
+using System.Collections.Generic;
+
+namespace Giao_Dien
+{
+    public partial class FormDangNhap : Form
+    {
+        bool isExit = true;
+        List<NguoiDung> _lstNguoiDung = DanhSachNguoiDung.Instance.LstNguoiDung;
+
+        public FormDangNhap()
+        {
+            InitializeComponent();
+        }
+        private void FormDangNhap_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (isExit)
+                Application.Exit();
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (KiemTraDangNhap(txtPass.Text, txtPass.Text))
+            {
+                MessageBox.Show("Đăng nhập thành công !", "Thông Báo");
+                switch (Const.NguoiDung.LoaiNguoiDung1)
+                {
+                    case NguoiDung.LoaiND.admin:
+                        FormQuanTriVien fQTV = new FormQuanTriVien();
+                        fQTV.ShowDialog();
+                        this.Hide();
+                        break;
+                    case NguoiDung.LoaiND.sinhvien:
+
+
+                        break;
+                    case NguoiDung.LoaiND.giangvien:
+
+
+                        break;
+                }
+            }
+            else if (txtUser.Text.Trim() == "" || txtPass.Text.Trim() == "")
+            {
+                MessageBox.Show("Username và password không được để trống !", "Thông báo");
+            }
+            else
+            {
+                MessageBox.Show("Sai username hoặc password !", "Thông Báo ");
+                txtUser.Focus();
+            }
+        }
+
+        bool KiemTraDangNhap(string username, string password)
+        {
+            for (int i = 0; i < _lstNguoiDung.Count; i++)
+            {
+                if (username == _lstNguoiDung[i].Username && password == _lstNguoiDung[i].Passsword)
+                {
+                    Const.NguoiDung = _lstNguoiDung[i];
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void FormDangNhap_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
